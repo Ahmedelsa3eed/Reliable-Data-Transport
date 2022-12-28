52c81500438a2975cd7ffe6158e39a6f79c70560
saeed <es-amgaballah24@alexu.edu.eg>
2022-12-28 14:57:21 +0200

    init commit

---
+++ client.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <time.h>
+#include <bitset>
+#include <cstring>
+#include <climits>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cout << "hello world\n";
+}
