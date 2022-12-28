91068169ec9e9c2c6551bb7f26def71c11cc90eb
saeed <es-amgaballah24@alexu.edu.eg>
2022-12-28 20:38:37 +0200

    prepare client using udp

--- client.cpp
+++ client.cpp
@@ -14,43 +14,52 @@
 #include <vector>
 #include <fstream>
 #include <string.h>
+#define MSS 500 // Maximum Segment Size
+#define MAXDATASIZE 1024 // 1k bytes: max number of bytes we can get at once
 
 using namespace std;
 
 typedef struct ack_packet {
-    long chsum;
-    long len;
-    long ackno;
+    uint16_t chsum;
+    uint16_t len;
+    uint16_t ackno;
 } ack_packet;
 
-typedef struct packet
-{
+typedef struct packet {
     /* Header */
     uint16_t chsum;
     uint16_t len;
     uint16_t seqno;
     /* Data */
-    char data[50];
+    char data[MSS];
 } packet;
 
-int sock_fd = 0, client_fd;
+int sock_fd = 0, client_fd, byte_count = 0;
 struct sockaddr_in serv_addr;
+socklen_t fromlen;
 vector<string> clientData(3); // <IP, port, filename>
 packet *pck;
+char buffer[MAXDATASIZE];
 
 void readClientData();
+void writeServerData();
+void receiveServerData();
 
 int main() {
     printf("Client: starting... \n");
     pck = (packet *) malloc(sizeof(packet));
 
+    // Creating socket file discriptor
     if ((sock_fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
     {
         printf("\n Socket creation error \n");
         return -1;
     }
+    
+    memset(&serv_addr, 0, sizeof(serv_addr));
 
     readClientData();
+    
     serv_addr.sin_family = AF_INET; // set to AF_INET to use IPv4
     serv_addr.sin_port = htons(stoi(clientData[1]));
     if (inet_pton(AF_INET, clientData[0].c_str(), &serv_addr.sin_addr) == -1)
@@ -60,7 +69,9 @@ int main() {
     }
 
     strcpy(pck->data, clientData[2].c_str());
+    pck->len = clientData[2].size();
 
+    // send the packet data directly for now
     if ((sendto(sock_fd, pck->data, pck->len, 0,
                 (struct sockaddr *)&serv_addr, sizeof(serv_addr))) == -1)
     {
@@ -68,14 +79,43 @@ int main() {
         return -1;
     }
 
+    receiveServerData();
+
+    free(pck);
     close(sock_fd);
 }
 
-void readClientData()
-{
+void readClientData() {
     ifstream infile("client.in");
     string line;
     int i = 0;
     while (getline(infile, line))
         clientData[i++] = line;
+}
+
+void receiveServerData() {
+    // open fileOutputStream for writing server data
+    ofstream wf("serverReceivedData.out", ios::out | ios::binary);
+    if (!wf) {
+        cout << "Cannot open file!" << endl;
+        return;
+    }
+    while (true) {
+        fromlen = sizeof serv_addr;
+        // receive data in buffer
+        if ((byte_count = recvfrom(sock_fd, buffer, sizeof buffer, 0,
+                                (struct sockaddr *)&serv_addr, &fromlen)) < 1)
+        {
+            printf("revfrom finished \n");
+            break;
+        }
+        printf("%d bytes of data in buf\n", byte_count);
+        // write received data
+        wf.write(buffer, byte_count);
+    }
+    wf.close();
+    if (!wf.good()) {
+        cout << "Error occurred at writing time!" << endl;
+        return;
+    }
 }
